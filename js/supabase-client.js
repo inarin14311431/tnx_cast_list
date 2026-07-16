4d132d4f358738e7135e625708701858e4e3763c
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-07-16 11:53:56 +0900

    Update supabase-client.js

---
+++ js/supabase-client.js
@@ -0,0 +1,17 @@
+import { createClient } from
+  "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const SUPABASE_URL = "https://koprmbkoftuuffslhsvt.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Dsb9Boo4aP3c_v-Iaam4mw_F1szMdUi";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
+);
