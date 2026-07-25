0e93fdb7efe326e19e132b9853e3e32e2cffaaea
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-07-25 17:15:15 +0900

    Add Intron monochrome theme support

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,7 @@
 /* Persistent color-theme controller shared by all active pages. */
 (() => {
   const STORAGE_KEY = "tnx-cast-site-theme";
-  const THEMES = new Set(["nova", "moon", "star", "eden", "vlad", "lutetia", "buena", "canberra"]);
+  const THEMES = new Set(["nova", "moon", "star", "eden", "vlad", "lutetia", "buena", "canberra", "intron"]);
   let buttonObserver = null;
   let buttonRefreshQueued = false;
 
@@ -51,7 +51,7 @@
     appendStylesheet("./css/theme-fixes-v3.css?v=2", "data-theme-fixes-v3");
     appendStylesheet("./css/theme-fixes-v4.css?v=4", "data-theme-fixes-v4");
     appendStylesheet("./css/style-skill-separators.css?v=1", "data-style-skill-separators");
-    appendStylesheet("./css/theme-city-expansion.css?v=1", "data-theme-city-expansion");
+    appendStylesheet("./css/theme-city-expansion.css?v=2", "data-theme-city-expansion");
   }
 
   function loadPageEnhancements(){
