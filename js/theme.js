8930ed4dc21eceb7befca7f20cfe74f7a9983b65
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-07-25 22:10:27 +0900

    イントロン認証配色CSSのキャッシュを更新

--- js/theme.js
+++ js/theme.js
@@ -52,7 +52,7 @@
     appendStylesheet("./css/theme-fixes-v4.css?v=4", "data-theme-fixes-v4");
     appendStylesheet("./css/style-skill-separators.css?v=1", "data-style-skill-separators");
     appendStylesheet("./css/theme-city-expansion.css?v=2", "data-theme-city-expansion");
-    appendStylesheet("./css/theme-intron-light.css?v=1", "data-theme-intron-light");
+    appendStylesheet("./css/theme-intron-light.css?v=2", "data-theme-intron-light");
     appendStylesheet("./css/theme-vlad-vampire.css?v=1", "data-theme-vlad-vampire");
     appendStylesheet("./css/theme-special-brands.css?v=2", "data-theme-special-brands");
     appendStylesheet("./css/mobile-auth-navigation-fix.css?v=1", "data-mobile-auth-navigation-fix");
