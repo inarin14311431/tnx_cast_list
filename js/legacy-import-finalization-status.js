58c2a6116e64f14722e8859fb8342d028b19a5fc
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-07-31 16:09:19 +0900

    Bump current TSV importer cache version

--- js/legacy-import-finalization-status.js
+++ js/legacy-import-finalization-status.js
@@ -4,7 +4,7 @@
 (() => {
   if (document.querySelector('script[data-current-tsv-import]')) return;
   const script = document.createElement('script');
-  script.src = './js/tsv-import-current.js?v=1';
+  script.src = './js/tsv-import-current.js?v=2';
   script.dataset.currentTsvImport = '1';
   document.head.append(script);
 })();
