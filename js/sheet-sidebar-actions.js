62394b40475b75edf8cd9acb9753a33077b95484
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-07-31 10:00:41 +0900

    Restore number spinners and visually center ability values

--- js/sheet-sidebar-actions.js
+++ js/sheet-sidebar-actions.js
@@ -89,9 +89,7 @@
       .ability-matrix--cs .ability-matrix__header,
       .ability-matrix--cs .ability-matrix__row{grid-template-columns:42px minmax(0,1fr);padding-left:4px;padding-right:4px}
       .ability-matrix--cs input[readonly]{cursor:default;background:rgba(65,232,255,.04)}
-      #ability-grid input[type="number"]{-moz-appearance:textfield;appearance:textfield}
-      #ability-grid input[type="number"]::-webkit-inner-spin-button,
-      #ability-grid input[type="number"]::-webkit-outer-spin-button{-webkit-appearance:none;margin:0}
+      #ability-grid .ability-matrix__row input[type="number"]{padding-left:19px}
       @media(min-width:1181px){.ability-grid{grid-template-columns:repeat(4,minmax(0,1fr)) minmax(110px,.5fr)!important}}
       @media(max-width:760px){
         .ability-matrix--cs .ability-matrix__header,
