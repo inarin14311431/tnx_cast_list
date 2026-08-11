0245791f60add8f450a403b42395b823ab5dcb74
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-11 20:33:20 +0900

    Sync master search access from validation

--- js/sheet-master-search-access.js
+++ js/sheet-master-search-access.js
@@ -1,4 +1,9 @@
 import { supabase } from "./supabase-client.js";
+import "./sheet-master-search-enhancements.js";
+import "./outfit-ofc-save.js";
+import "./outfit-ofc-tsv.js";
+import "./outfit-ofc-master-apply.js";
+import "./outfit-ofc-tsv-category-normalize.js";
 
 const selectors = ["#search-skd-master", "#search-ofc-master"];
 const buttons = selectors.map(selector => document.querySelector(selector)).filter(Boolean);
